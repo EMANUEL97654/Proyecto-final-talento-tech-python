05a8e0c899bab460b9927956ddb439ead95265fb
Emanuel <gonzlezgartlandemanuel@gmail.com>
2026-05-09 15:17:20 -0300

    Se agrega try-except al menu

--- database.py
+++ database.py
@@ -4,15 +4,20 @@ def conectar():
     return sql.connect("inventario.db")
 
 def crear_tabla():
-    conexion = conectar()
-    cursor = conexion.cursor()
-    cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS productos(
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       nombre TEXT NOT NULL,
-                       descripcion TEXT,
-                       cantidad INTEGER NOT NULL,
-                       precio REAL NOT NULL,
-                       categoria TEXT)''')
-    conexion.commit()
-    conexion.close()
+    try:
+        conexion = conectar()
+        cursor = conexion.cursor()
+        cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS productos(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nombre TEXT NOT NULL,
+                        descripcion TEXT,
+                        cantidad INTEGER NOT NULL,
+                        precio REAL NOT NULL,
+                        categoria TEXT)''')
+        conexion.commit()
+    except sql.Error as e:
+        print(f"ERROR: {e}")
+    finally:
+        if conexion:
+            conexion.close()
--- menu.py
+++ menu.py
@@ -11,48 +11,54 @@ from productos import (
 
 def mostrar_menu():
     while True:
-        print("\n===== SISTEMA DE INVENTARIO =====")
-        print("1. Agregar producto")
-        print("2. Mostrar productos")
-        print("3. Buscar producto por ID")
-        print("4. Buscar producto por categoría")
-        print("5. Buscar producto por nombre")
-        print("6. Eliminar producto según ID")
-        print("7. Actualizar producto")
-        print("8. Reporte de stock bajo")
-        print("9. Salir")
+        try:
+            print("\n===== SISTEMA DE INVENTARIO =====")
+            print("1. Agregar producto")
+            print("2. Mostrar productos")
+            print("3. Buscar producto por ID")
+            print("4. Buscar producto por categoría")
+            print("5. Buscar producto por nombre")
+            print("6. Eliminar producto según ID")
+            print("7. Actualizar producto")
+            print("8. Reporte de stock bajo")
+            print("9. Salir")
 
-        opcion = input("Seleccione una opción: ")
+            opcion = input("Seleccione una opción: ").strip()
 
-        match(opcion):
-            case "1":
-                agregar_productos()
-                continue
-            case "2":
-                mostrar_productos()
-                continue
-            case "3":
-                buscar_producto_por_id()
-                continue
-            case "4":
-                buscar_producto_por_categoria()
-                continue
-            case "5":
-                buscar_producto_por_nombre()
-                continue
-            case "6":
-                eliminar_producto_segun_id()
-                continue
-            case "7":
-                actualizar_productos()
-                continue
-            case "8":
-                reporte_stock_bajo()
-                continue
-            case "9":
-                print("Saliendo del programa...")
-                break
-            case _:
-                print("ERROR. Ingrese un numero dentro del menú: ")
-                continue
+            match(opcion):
+                case "1":
+                    agregar_productos()
+                    continue
+                case "2":
+                    mostrar_productos()
+                    continue
+                case "3":
+                    buscar_producto_por_id()
+                    continue
+                case "4":
+                    buscar_producto_por_categoria()
+                    continue
+                case "5":
+                    buscar_producto_por_nombre()
+                    continue
+                case "6":
+                    eliminar_producto_segun_id()
+                    continue
+                case "7":
+                    actualizar_productos()
+                    continue
+                case "8":
+                    reporte_stock_bajo()
+                    continue
+                case "9":
+                    print("Saliendo del programa...")
+                    break
+                case _:
+                    print("ERROR. Ingrese un numero dentro del menú: ")
+                    continue
+        except KeyboardInterrupt:
+            print("\n El programa fue interrumpido por el usuario.")
+            break
+        except Exception as e:
+            print(f"Ocurrio un error inesperado: {e}")
     
