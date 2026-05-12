70c135293104d6d3f576526e459d2bed093c7d61
Emanuel <gonzlezgartlandemanuel@gmail.com>
2026-05-12 15:49:51 -0300

    Se agrega docstrings para documentar

--- database.py
+++ database.py
@@ -1,9 +1,30 @@
 import sqlite3 as sql
 
+"""
+Módulo encargado de la conexión y creación
+de la base de datos SQLite.
+
+"""
+
 def conectar():
+    '''
+    Crea y retorna una conexión a la base de datos.
+    '''
+    
     return sql.connect("inventario.db")
 
 def crear_tabla():
+    """
+    Crea la tabla productos si todavía no existe.
+    La tabla almacena:
+    - ID
+    - Nombre
+    - Descripción
+    - Cantidad
+    - Precio
+    - Categoría
+    """
+    
     try:
         conexion = conectar()
         cursor = conexion.cursor()
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+Archivo principal del sistema de inventario.
+
+Este archivo inicia la aplicación:
+- Crea la tabla si no existe.
+- Ejecuta el menú principal.
+"""
+
 from database import crear_tabla
 from menu import mostrar_menu
 
